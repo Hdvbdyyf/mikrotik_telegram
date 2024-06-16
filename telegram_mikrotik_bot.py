0b7a5a3a2d2afb7a4788c949f806251592317055
Flavio Pace <flavio.pace@gmail.com>
2024-06-16 22:28:56 +0200

    added class

--- telegram_mikrotik_bot.py
+++ telegram_mikrotik_bot.py
@@ -2,19 +2,30 @@ from librouteros import connect
 from getpass import getpass
 import json
 
-ip_address = ["192.168.1.254"]
-user = 'admin'
-passw = getpass()
-for ip in ip_address:
-    api = connect(username=user, password=passw, host=ip)
-    ip_info = api(cmd="/ip/address/print")
-    a = tuple(ip_info)
-    # This also will work, as well as anything else you can do with iterables
-    for item in a:
-        print(item)
+
+class Mikrotik:
+    def __init__(self, ipaddr, username, password):
+        self.ip = ipaddr
+        self.user = username
+        self.password = password
+        self.con = self.connect()
+
+    def connect(self):
+        return connect(username=self.user, password=self.password, host=self.ip)
+
+    def getIpAddresses(self):
+        ip_info = self.con(cmd="/ip/address/print")
+        a = tuple(ip_info)
+        # This also will work, as well as anything else you can do with iterables
+        for item in a:
+            print(item)
+
+    def getInterfaces(self):
+        interfaces = self.con.path('interface')
 
 
-interfaces = api.path('interface')
 
-#for item in interfaces:
-    #print(item)
+if __name__ == "__main__":
+
+    mik = Mikrotik(ipaddr='192.168.1.254', username='admin', password='demo') 
+    mik.getIpAddresses()
