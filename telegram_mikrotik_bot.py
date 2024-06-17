6064fd3387c43ed6aea76ea94ba31292004ee2ba
Flavio Pace <flavio.pace@gmail.com>
2024-06-17 21:53:43 +0200

    starting to integrate also json

--- telegram_mikrotik_bot.py
+++ telegram_mikrotik_bot.py
@@ -1,6 +1,10 @@
+import sys
 from librouteros import connect
 from getpass import getpass
 import json
+import os
+
+JSON_FILE = 'config.json'
 
 
 class Mikrotik:
@@ -27,5 +31,11 @@ class Mikrotik:
 
 if __name__ == "__main__":
 
-    mik = Mikrotik(ipaddr='192.168.1.254', username='admin', password='demo') 
+    with open(os.path.join(sys.path[0], JSON_FILE), 'r') as in_file:
+        conf = json.load(in_file)
+
+    ip = conf['mikrotik_login']['ip']
+    user = conf['mikrotik_login']['username']
+    passwd = conf['mikrotik_login']['password']
+    mik = Mikrotik(ipaddr=ip, username=user, password=passwd) 
     mik.getIpAddresses()
